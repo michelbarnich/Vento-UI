47555dfbb2b1b4fad732ad4772a9cfff5d6f7cd4
Michel Barnich <private@michelbarnich.com>
2020-04-14 13:14:50 +0200

    Initial Commit

---
+++ Vento/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Vento
+//
+//  Created by Michel Barnich on 14/04/2020.
+//  Copyright © 2020 Michel Barnich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
